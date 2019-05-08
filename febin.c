6e4dbd741fefc1d9aefb50245cf66058b7777a5d
Febin140603 <50397492+Febin140603@users.noreply.github.com>
2019-05-08 14:40:47 +0530

    Update febin.c

--- febin.c
+++ febin.c
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -8,10 +7,12 @@ void print(int arr[], int n)
 		cout << arr[i] << " ";
 	}
 }
-void reverse(int arr[], int n)
+void reverse(int arr[], int low, int high)
 {
-	for (int low = 0, high = n - 1; low < high; low++, high--) {
+	if (low < high)
+	{
 		swap(arr[low], arr[high]);
+		reverse(arr, low + 1, high - 1);
 	}
 }
 int main()
@@ -19,7 +20,7 @@ int main()
 	int arr[] = { 1, 2, 3, 4, 5 };
 	int n = sizeof(arr)/sizeof(arr[0]);
 
-	reverse(arr, n);
+	reverse(arr, 0, n-1);
 	print(arr, n);
 
 	return 0;

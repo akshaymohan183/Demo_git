0cd52ead50a0ee702c8305a3879f2bd736442cc7
himm26 <42342317+himm26@users.noreply.github.com>
2019-05-08 14:53:35 +0530

    Update himm.c

--- himm.c
+++ himm.c
@@ -1,28 +1,18 @@
-#include<iostream>
+
+#include <bits/stdc++.h>
 using namespace std;
-void selectionSort(int a[], int n) {
-   int i, j, min, temp;
-   for (i = 0; i < n - 1; i++) {
-      min = i;
-      for (j = i + 1; j < n; j++)
-         if (a[j] < a[min])
-            min = j;
-      temp = a[i];
-      a[i] = a[min];
-      a[min] = temp;
-   }
-}
-int main() {
-   int a[] = { 22, 91, 35, 78, 10, 8, 75, 99, 1, 67 };
-   int n = sizeof(a)/ sizeof(a[0]);
-   int i;
-   cout<<"Given array is:"<<endl;
-   for (i = 0; i < n; i++)
-   cout<< a[i] <<" ";
-   cout<<endl;
-   selectionSort(a, n);
-   cout<<"\nSorted array is: \n";
-   for (i = 0; i < n; i++)
-   cout<< a[i] <<" ";
-   return 0;
+
+int main()
+{
+	int arr[] = {1, 5, 8, 9, 6, 7, 3, 4, 2, 0};
+	int n = sizeof(arr)/sizeof(arr[0]);
+
+	sort(arr, arr+n);
+
+	cout << "\nArray after sorting using "
+		"default sort is : \n";
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+
+	return 0;
 }

cf0d905741cbab2cd0258f92b0a06559afa2c353
Febin140603 <50397492+Febin140603@users.noreply.github.com>
2019-05-08 14:20:29 +0530

    Update febin.c

--- febin.c
+++ febin.c
@@ -1,1 +1,25 @@
-print("hjgfhjfdh");
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void print(int arr[], int n)
+{
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+}
+void reverse(int arr[], int n)
+{
+	for (int low = 0, high = n - 1; low < high; low++, high--) {
+		swap(arr[low], arr[high]);
+	}
+}
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5 };
+	int n = sizeof(arr)/sizeof(arr[0]);
+
+	reverse(arr, n);
+	print(arr, n);
+
+	return 0;
+}
